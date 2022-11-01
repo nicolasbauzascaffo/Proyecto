b8be12f7afaba1cc2c5901781452eac10ee050fa
nicolasbauzascaffo <112832033+nicolasbauzascaffo@users.noreply.github.com>
2022-10-31 21:20:03 -0300

    Add files via upload

---
+++ lista.js
@@ -0,0 +1,11 @@
+const lista = {
+    nombre: "Nicolas",
+    edad: 29,
+    eresDesarrolado: true,
+    anionacimiento: new Date (1993,9,26),
+    libroFavorito: ["Harry Potter", "J. K. Rowling"]
+}
+
+console.log (lista)
+    
+
