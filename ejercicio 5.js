e91f4ea649eeea1d7750ff4dbaa84649eeaca3da
Nicolás Bauzá <112832033+nicolasbauzascaffo@users.noreply.github.com>
2022-11-02 12:25:50 -0300

    Add files via upload

---
+++ ejercicio 5.js
@@ -0,0 +1,13 @@
+var altura = 189;
+var alturametros = (altura / 100);
+console.log (alturametros)
+var peso = 90.50;
+var alturaredondeado = (((altura.toFixed(1)))/100);
+console.log (alturaredondeado);
+var alturaarriba = (Math.floor (peso));
+console.log (alturaarriba);
+
+var max = (Number.MAX_VALUE) + 1 ;
+var maxnuevo = max.toString ();
+var final = (maxnuevo = (Number.MAX_VALUE))
+console.log (maxnuevo = (Number.MAX_VALUE))
