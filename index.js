74454b8a13aa87ec773f22c4a6073f9cec863a29
Nicolás Bauzá <112832033+nicolasbauzascaffo@users.noreply.github.com>
2022-11-03 17:23:57 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log ("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo")
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.Console (),
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+module.exports = logger;
+
+
+try {
+    console.info ("este es mi mensaje personalizado")
+
+} catch (error){
+    console.log ("Error")
+};
+
