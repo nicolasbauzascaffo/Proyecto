b6589fa5410e344b2b5482c08027bf717ac3d0c9
Nicolás Bauzá <112832033+nicolasbauzascaffo@users.noreply.github.com>
2022-11-03 10:18:19 -0300

    Add files via upload

---
+++ conjuntos.js
@@ -0,0 +1,10 @@
+const familia = ["Miriam" , "Walter" , "Antonella", "Nicolás"];
+const flia1 = new Set (familia);
+console.log (flia1);
+flia1.add ("Nicolás");
+console.log (flia1);
+flia1.add ("JavaScript");
+console.log (flia1);
+
+
+
