414118ddd49d0be2f80bfc4079cbfc6a620cfa49
Nicolás Bauzá <112832033+nicolasbauzascaffo@users.noreply.github.com>
2022-11-03 10:55:48 -0300

    Add files via upload

---
+++ consola.js
@@ -0,0 +1,12 @@
+prompt ("Cual es tu edad");
+
+const edad = ("¿Cuál es tu edad?");
+
+const edad = prompt ("¿Cuál es tu edad?");
+
+console.log (edad)
+
+
+const numero = Number (edad);
+
+console.log (numero)
