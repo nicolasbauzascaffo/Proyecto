2e740e96e408d4fa67c6b6c5005d4f74082a49eb
Nicolás Bauzá <112832033+nicolasbauzascaffo@users.noreply.github.com>
2022-11-04 19:23:05 -0300

    Add files via upload

---
+++ proyectonode/controller.js
@@ -0,0 +1,8 @@
+export function sumar (a,b) {
+    return a + b
+}
+
+
+export function multiplicar (a,b) {
+    return a * b
+}
---
+++ proyectonode/entrypoint.js
@@ -0,0 +1,10 @@
+import { sumar, multiplicar } from './controller.js'
+
+const suma1 = sumar (1,2);
+console.log (suma1);
+
+const suma2 = sumar (4,5);
+console.log (suma2);
+
+const multiplica = multiplicar (suma1,suma2);
+console.log (multiplica);
